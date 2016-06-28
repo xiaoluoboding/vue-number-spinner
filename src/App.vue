c0ffee01cd9695153e93b8921d631d23eb19a605
xiaoluoboding <xiaoluoboding@gmail.com>
2016-06-28 22:55:54 +0800

    Finish first edition

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script>
-import Spinner from './Spinner.vue'
+import vueNumberSpinner from './vue-number-spinner.vue'
 
 export default {
   data () {
     return {
-      msg: 'vue-number-spinner'
+      title: 'vue-number-spinner',
+      stepByOne: '<vue-number-spinner :min=0 :max=10 :step=1></vue-number-spinner>'
     }
   },
 
   components: {
-    Spinner
+    'vue-number-spinner': vueNumberSpinner
   }
 }
 </script>
@@ -17,20 +18,23 @@ export default {
 <template>
   <div id="app">
     <div id="wrapper">
-      <h1>{{ msg }}</h1>
+      <h1><span v-text="title"></span></h1>
       <a href="#!" class="button">Source on GitHub</a>
       <div class="demo-container">
-        <section class="desc">
-          <p>说明：vue.js版本的number spinner组件，支持按钮、键盘、鼠标滑轮操作。</p>
+        <section class="demo-desc">
+          <!-- <p>基于Vue.js的number spinner组件，支持按钮点击、键盘<kbd>↑</kbd><kbd>↓</kbd>、鼠标滚轮事件。</p> -->
+          <p>Number spinner component for Vue.js, Easy to use.</p>
+          <p>Support button click、keyboard <kbd>↑</kbd><kbd>↓</kbd> and mousewheel Event.</p>
         </section>
-        <section class="demo">
-          <div class="number">
-            <label class="for-spinner">Number:</label>
-            <spinner :min=-5 :max=25 :step=5></spinner>
+        <hr>
+        <section class="demo-list">
+          <div class="spinner-group number">
+            <label class="label-control">Step By 1:</label>
+            <vue-number-spinner :min=0 :max=10 :step=1></vue-number-spinner>
           </div>
-          <div class="currency">
-            <label class="for-spinner">Currency:</label>
-            <spinner :min=0 :max=10 :step=0.1></spinner>
+          <div class="spinner-group number">
+            <label class="label-control">Step By 5:</label>
+            <vue-number-spinner :min=0 :max=100 :step=5></vue-number-spinner>
           </div>
         </section>
       </div>
@@ -54,75 +58,74 @@ html {
 }
 
 body {
-    font-family: 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
-    font-size: 15px;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #34495e;
-    margin: 0;
-    border-top: 2px solid #4fc08d;
-    background-color: #f8f8f8;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
+  font-family: 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
+  font-size: 15px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #34495e;
+  margin: 0;
+  border-top: 2px solid #4fc08d;
+  background-color: #f8f8f8;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h1 {
-    font-family: 'Dosis', 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
-    font-weight: 300;
-    font-size: 4em;
-    color: #2c3e50;
+  font-family: 'Dosis', 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
+  font-weight: 300;
+  font-size: 4em;
+  color: #2c3e50;
 }
 
 h4, a {
-    font-weight: 400;
-    font-size: 15px;
-    color: #7f8c8d;
+  font-weight: 400;
+  font-size: 15px;
+  color: #7f8c8d;
 }
 
 p {
-    word-spacing: 0.05em;
+  word-spacing: 0.05em;
 }
 
 a {
-    text-decoration: none;
-    color: #34495e;
+  text-decoration: none;
+  color: #34495e;
 }
 
-
 a.button {
-    display: inline-block;
-    width: 280px;
-    margin: 0.5em;
-    font-family: 'Roboto Mono', Monaco, courier, monospace;
-    font-weight: 700;
-    color: #fff;
-    background-color: #4fc08d;
-    border-bottom: 2px solid #3aa373;
-    padding: 12px 14px;
-    border-radius: 4px;
-    transition: all 0.15s ease;
+  display: inline-block;
+  width: 280px;
+  margin: 0.5em;
+  font-family: 'Roboto Mono', Monaco, courier, monospace;
+  font-weight: 700;
+  color: #fff;
+  background-color: #4fc08d;
+  border-bottom: 2px solid #3aa373;
+  padding: 12px 14px;
+  border-radius: 4px;
+  transition: all 0.15s ease;
 }
 
 a.button:hover {
-    background-color: #5dc596;
-    -webkit-transform: translate(0, -2px);
-            transform: translate(0, -2px);
+  background-color: #5dc596;
+  -webkit-transform: translate(0, -2px);
+          transform: translate(0, -2px);
 }
 
 #social {
-    text-align: center;
-    list-style-type: none;
-    margin: 0 auto;
-    padding: 0;
-    margin: 1.5em auto;
+  text-align: center;
+  list-style-type: none;
+  margin: 0 auto;
+  padding: 0;
+  margin: 1.5em auto;
 }
 
 #wrapper {
-    width: 650px;
-    margin: 0 auto;
-    min-height: 80px;
-    text-align: center;
+  width: 650px;
+  margin: 0 auto;
+  min-height: 80px;
+  text-align: center;
 }
 
 .demo-container {
@@ -130,36 +133,70 @@ a.button:hover {
   border-radius: 2px;
   background-color: #FBFBFB;
   width: 100%;
+  margin-top: 64px;
   padding: 10px;
 }
 
 .block{
-    margin-top: 25px;
+  margin-top: 25px;
 }
 
 .reset-btn{
-    outline: 0;
-    cursor: pointer;
-    padding: 7px 15px;
-    border-radius: 2px;
-    background-color: #58DCA1;
-    color: #fff;
-    font-weight: bold;
+  outline: 0;
+  cursor: pointer;
+  padding: 7px 15px;
+  border-radius: 2px;
+  background-color: #58DCA1;
+  color: #fff;
+  font-weight: bold;
 }
 
-.for-spinner {
+.label-control {
+  position: relative;
+  min-height: 1px;
+  padding-left: 12px;
+  width: 20%;
+  text-align: right;
   float: left;
   margin: 4px 20px;
 }
 
+.spinner-group {
+  margin-bottom: 15px;
+}
+
+.demo-desc {
+  margin-bottom: 30px;
+}
+
+.demo-list {
+  margin-top: 30px;
+}
+
+kbd {
+  margin: 2px;
+  padding: 2px 4px;
+  font-size: 90%;
+  color: #fff;
+  background-color: #333;
+  border-radius: 3px;
+  -webkit-box-shadow: inset 0 -1px 0 rgba(0,0,0,.25);
+  box-shadow: inset 0 -1px 0 rgba(0,0,0,.25);
+  vertical-align: middle;
+}
+
+hr {
+  color: #eee;
+  opacity: .1;
+}
 
 @media (max-width: 700px) {
-    html,
-    body {
-        margin: 0;
-    }
-    #wrapper {
-        width: 90%;
-    }
+  html,
+  body {
+      margin: 0;
+  }
+  #wrapper {
+      width: 90%;
+  }
 }
 </style>
--- src/vue-number-spinner.vue
+++ src/vue-number-spinner.vue
@@ -1,6 +1,5 @@
 <script>
 	export default {
-		name: 'Spinner',
 		data () {
 			return {
 				val: 0,
@@ -23,7 +22,7 @@
 		},
 
 		computed: {
-			
+
 		},
 
 		ready () {
@@ -58,17 +57,16 @@
 <template>
 	<div class="input-group">
 		<span class="input-group-btn">
-			<button class="btn-minus" @click="minus()"><span>-</span></button>
+			<button class="btn btn-minus" @click="minus()"><span>-</span></button>
 		</span>
-		<input type="text" class="spinner" v-model="val" @keydown.up="plus" @keydown.down="minus" @wheel="onWheel">
+		<input type="text" class="number-spinner" v-model="val" @keydown.up="plus" @keydown.down="minus" @wheel="onWheel">
 		<div class="input-group-btn">
-			<button class="btn-plus" @click="plus()"><span>+</span></button>
+			<button class="btn btn-plus" @click="plus()"><span>+</span></button>
 		</div>
 	</div>
 </template>
 
 <style>
-
 .input-group {
   position: relative;
   display: table;
@@ -82,43 +80,32 @@
   white-space: nowrap;
 }
 
-.btn-plus {
-  display: table-cell;
+.btn {
+	display: table-cell;
   font-family: 'Roboto Mono', Monaco, courier, monospace;
   font-weight: 700;
   color: #fff;
   height: 32px;
+	border: none;
+}
+
+.btn-plus {
   background-color: #4fc08d;
-  border-bottom: 2px solid #3aa373;
-  border: none;
-  transition: all 0.15s ease;
 }
 
 .btn-plus:hover {
   background-color: #5dc596;
-  /*-webkit-transform: translate(0, -2px);
-          transform: translate(0, -2px);*/
 }
 
 .btn-minus {
-  display: table-cell;
-  font-family: 'Roboto Mono', Monaco, courier, monospace;
-  font-weight: 700;
-  color: #fff;
-  height: 32px;
   background-color: #f66;
-  border: none;
-  /*padding: 2px;*/
-  /*transition: all 0.15s ease;*/
 }
 
 .btn-minus:hover {
   background-color: #f56;
-  /*-webkit-transform: translate(0, -2px);
-          transform: translate(0, -2px);*/
 }
 
-.spinner {
+.number-spinner {
   height: 32px;
   line-height: 32px;
   box-sizing: border-box;
@@ -128,7 +115,7 @@
   transition: border-color 0.2s ease;
 }
 
-.spinner:focus {
+.number-spinner:focus {
 	border: 1px solid #4fc08d;
 }
 </style>
